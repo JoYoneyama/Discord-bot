de8184c2ac3652641ff149daf9d9109b9c489a79
JoYoneyama <jvsyoneyama@gmail.com>
2025-02-10 14:07:34 -0300

    funcao de musica funcionando com commands

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 import discord
 
 from dotenv import load_dotenv
-from discord import app_commands
+from yt_dlp import YoutubeDL
+from discord import app_commands, FFmpegPCMAudio
 from discord.ext import commands
 
 
@@ -20,11 +21,39 @@ class MyClient(commands.Bot):
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = MyClient(command_prefix="!", intents=intents)
+client = MyClient(command_prefix="@Testando", intents=intents)
+
+# comandos do bot
 
 @client.command()
 async def falar(ctx, string=""):
     await ctx.send(f"falando {string}")
 
+@client.command()
+async def play(ctx, url):
+    if ctx.author.voice:
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+
+        ydl_opts = {
+            "format": "bestaudio"
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            audio_url = info["url"]
+
+        source = FFmpegPCMAudio(audio_url)
+        voice.play(source)        
+    else:
+        await ctx.send("Você precisa estar em um canal de voz!")
+
+@client.command()
+async def disconnect(ctx):
+    if ctx.voice_client:
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Vazei")
+
+# ---------------------
 
 client.run(token)
