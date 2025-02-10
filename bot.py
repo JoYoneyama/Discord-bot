2598ddb2b008e06e10cad728f78cec9fbf86b702
JoYoneyama <jvsyoneyama@gmail.com>
2025-02-10 16:05:29 -0300

    mudando para o bot ser chamado por @Testando

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
 import discord
 
+from extension import *
 from dotenv import load_dotenv
 from yt_dlp import YoutubeDL
 from discord import app_commands, FFmpegPCMAudio
@@ -11,49 +13,49 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
 
-class MyClient(commands.Bot):
+class MyClient(discord.Client):
     async def on_ready(self):
-        print(f"Logged on as {self.user}!")
+        print(f"Logado como {self.user}!")
 
-    # async def on_message(self, message):
-    #     print(f"message from {message.author}: {message.content}")        
+    async def on_message(self, command):       # todos os comandos do bot
+        if self.user.mention in command.content:           # bot acionado ao ser mencionado
+            command_content = command.content[len(self.user.mention):].strip()  # o content possui o id do bot q foi mencionado, ent tem q tirar
+
+             # comandos do bot
+
+            if "play" in command_content:
+                url = command_content[len("play"):].strip()
+
+                if command.author.voice:
+                    if not command.guild.voice_client:
+                        channel = command.author.voice.channel
+                        voice = await channel.connect()
+                    else:                                      # se o bot ja estiver no canal
+                        voice = command.guild.voice_client  
+
+                    # tocando musica
+
+                    ydl_opts = {
+                        "format": "bestaudio"
+                    }
+
+                    with YoutubeDL(ydl_opts) as ydl:
+                        info = ydl.extract_info(url, download=False)
+                        audio_url = info["url"]
+
+                    source = FFmpegPCMAudio(audio_url)
+                    voice.play(source)  
+                else:
+                    await command.channel.send("Você precisa estar em um canal de voz!")
+
+            if "disconnect" in command_content:
+                if command.guild.voice_client:
+                    await command.guild.voice_client.disconnect()
+                    await command.channel.send("Vazei")
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = MyClient(command_prefix="@Testando", intents=intents)
 
-# comandos do bot
-
-@client.command()
-async def falar(ctx, string=""):
-    await ctx.send(f"falando {string}")
-
-@client.command()
-async def play(ctx, url):
-    if ctx.author.voice:
-        channel = ctx.message.author.voice.channel
-        voice = await channel.connect()
-
-        ydl_opts = {
-            "format": "bestaudio"
-        }
-
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            audio_url = info["url"]
-
-        source = FFmpegPCMAudio(audio_url)
-        voice.play(source)        
-    else:
-        await ctx.send("Você precisa estar em um canal de voz!")
-
-@client.command()
-async def disconnect(ctx):
-    if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
-        await ctx.send("Vazei")
-
-# ---------------------
-
+client = MyClient(intents=intents)
 client.run(token)
